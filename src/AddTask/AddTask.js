fc140d176b4c3ecda30baaabde33c676280eab72
Dane <soredt1@student.op.ac.nz>
2023-03-27 20:13:47 +1300

    created some basic security rules, think i have a bug with logging out now tho

--- src/AddTask/AddTask.js
+++ src/AddTask/AddTask.js
@@ -2,53 +2,74 @@
 Add Task component.
 Click add task to add a new title and description of task
 */
+import Modal from "../Modal/Modal";
+import { useState } from "react";
+import "./addTask.css";
+import { db } from "../firebase"; // Importing the Firebase database configuration
+import {
+  serverTimestamp,
+  doc,
+  collection,
+  addDoc,
+  getDoc
+} from "firebase/firestore"; // Importing Firebase Firestore functionalities
 
-import Modal from "../Modal/Modal"
-import {useState} from 'react'
-import './addTask.css'
-import {db} from '../firebase' // Importing the Firebase database configuration
-import {collection, addDoc, Timestamp} from 'firebase/firestore' // Importing Firebase Firestore functionalities
-
-function AddTask({onClose, open}) {
-
+function AddTask({ onClose, open, userUid }) {
   // Create state variables for title and description of the tasks
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
-
-  // Function to add new task to firestore
-  const handleSubmit = async (e) => { // Receive event
-    e.preventDefault() // Prevent default behaviour 
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
     try {
-      await addDoc(collection(db, 'tasks'), { // addDoc function to add a new document to the 'tasks' collection in Firestore
+      const userDocRef = doc(db, "users", userUid);
+      const tasksCollectionRef = collection(userDocRef, "tasks");
+      const newTaskDocRef = await addDoc(tasksCollectionRef, {
         title: title,
         description: description,
         completed: false,
-        created: Timestamp.now()
-      })
-      onClose() 
-    } catch (err) {
-      alert(err)
+        created: serverTimestamp(),
+      });
+      console.log("New task created with ID: ", newTaskDocRef.id);
+  
+      // Get the new task document with taskId
+      const taskDocRef = doc(db, "users", userUid, "tasks", newTaskDocRef.id);
+      const taskDoc = await getDoc(taskDocRef);
+      console.log("Task data: ", taskDoc.data());
+  
+      onClose();
+    } catch (error) {
+      console.error("Error adding document: ", error);
     }
-  }
+  };
 
   return (
-    <Modal modalLabel='Add Task' onClose={onClose} open={open}> {/* Render in the Modal component with props modalLabel, onClose, and open */}
-      <form onSubmit={handleSubmit} className='addTask' name='addTask'>
-        <input 
-          type='text' 
-          name='title' 
+    <Modal modalLabel="Add Task" onClose={onClose} open={open}>
+      {/* Render in the Modal component with props modalLabel, onClose, and open */}
+      <form onSubmit={handleSubmit} className="addTask" name="addTask">
+        <input
+          type="text"
+          name="title"
           onChange={(e) => setTitle(e.target.value.toUpperCase())}
           value={title}
-          placeholder='Enter title'/>
-        <textarea 
-          name='description'
+          placeholder="Enter title"
+        />
+        <textarea
+          name="description"
           onChange={(e) => setDescription(e.target.value)}
-          placeholder='Enter task description'
-          value={description}></textarea>
-        <button type='submit' name='addTask'>Done</button> {/* Button that will trigger the handleSubmit function when clicked */}
-      </form> 
+          placeholder="Enter task description"
+          value={description}
+        ></textarea>
+        <button type="submit" name="addTask">
+          Done
+        </button>
+        {/* Button that will trigger the handleSubmit function when clicked */}
+      </form>
     </Modal>
-  )
+  );
 }
 
 export default AddTask
+
+
--- src/TaskManager/TaskManager.js
+++ src/TaskManager/TaskManager.js
@@ -47,7 +47,7 @@ function TaskManager() {
 
   // function to get all tasks from firestore in realtime 
   useEffect(() => {
-    const taskColRef = query(collection(db, 'tasks'), orderBy('created', 'desc')) // Firestore query to get all tasks and order by created date in descending order
+    const taskColRef = query(collection(db, 'users', user?.uid, 'tasks'), orderBy('created', 'desc')) // Firestore query to get all tasks and order by created date in descending order
     const unsubscribe = onSnapshot(taskColRef, (snapshot) => { // Listen for changes to Firestore task collection
       setTasks(snapshot.docs.map(doc => ({ // Update tasks state variable with data from each Firestore document
         id: doc.id,
@@ -57,48 +57,50 @@ function TaskManager() {
     return () => {
       unsubscribe() // Unsubscribe from the onSnapshot listener when the component unmounts
     }
-  },[])
+  },[user?.uid])
 
   return (
     <div className='taskManager'>
-    <header>Task Manager</header>
-    <button className="logout_btn" onClick={logout}>
-      Logout
-    </button>
-    <div className="userDetails">
-      Logged in as
-      <div>{name}</div>
-      <div>{user?.email}</div>
-    </div>
-    <div className='taskManager__container'>
-      <button 
-        name='Add task +'
-        onClick={() => setOpenAddModal(true)}>
-        Add task +
+      <header>Task Manager</header>
+      <button className="logout_btn" onClick={logout}>
+        Logout
       </button>
-      <div className='taskManager__tasks'>
-
-        {/* Map over all tasks and create a Task component for each one */}
-        {tasks.map((task) => (
-          <Task
-            id={task.id}
-            key={task.id}
-            completed={task.data.completed}
-            title={task.data.title} 
-            description={task.data.description}
-          />
-        ))}
-
+      <div className="userDetails">
+        Logged in as
+        <div>{name}</div>
+        <div>{user?.email}</div>
       </div>
-    </div>
-
+      <div className='taskManager__container'>
+        <button 
+          name='Add task +'
+          onClick={() => setOpenAddModal(true)}>
+          Add task +
+        </button>
+        <div className='taskManager__tasks'>
+  
+          {/* Map over all tasks and create a Task component for each one */}
+          {tasks.map((task) => (
+            <Task
+              id={task.id}
+              key={task.id}
+              completed={task?.data?.completed}
+              title={task?.data?.title} 
+              description={task?.data?.description}
+              userUid={user?.uid}
+              taskDocId={task.id}
+            />
+          ))}
+  
+        </div>
+      </div>
+  
       {/* Render the AddTask component if the 'openAddModal' state variable is true */}
       {openAddModal &&
-        <AddTask onClose={() => setOpenAddModal(false)} open={openAddModal}/>
+        <AddTask onClose={() => setOpenAddModal(false)} open={openAddModal} userUid={user?.uid}/>
       }
-
+  
     </div>
-  )
+  )  
 }
 
 export default TaskManager
